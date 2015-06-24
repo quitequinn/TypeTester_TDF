fb2ffd99ba0d74b772ba1b7683085dfec7e78544
qkeave <qkeave@gmail.com>
2015-06-24 14:52:57 -0700

    MINIFIED AND CLOSURE COMPILED

--- build/js/scripts_forCloserCompiler.js
+++ build/js/scripts_forCloserCompiler.js
@@ -20,16 +20,16 @@ function sized(){
 var testerNumber = testerTotal = 0;
 
 function typetester(e, font, size, tracking, italic,  weight, opt){
-	var option, typetester;
+	var option, options, typetester;
 	var theTester = e;
 	var size =
 		font =
-		options =
 		tracking =
 		weight =
 		italic =
 		opt =
 		align =
+		singleline =
 		testerfit =
 		testerSize =
 		testerFont =
@@ -38,17 +38,25 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 		testerItalic =
 		testerOpt =
 		testerAlign =
+		testerSingleline =
 		sizeoptions =
 		trackingoptions =
 		weightoptions =
 		italicoptions =
+		singlelineoptions =
+		alignoptions =
 		optoptions =
 		optButton =
-		testeroptions = "";
+		testeroptions =
+		weightselection =
+		weightPosition = "";
+	var obj_weightselection = {};
 	var elm = 0;
 	var text = "Try typing here...";
 	var yes = [ "yes", "true", "hawt", "yup", "yep", "siq", "swell", "chiller", "ok", "!", "fine", "right", "good", "aye", "indubitably", "sure", "yeah", "yay"];
 	var no = [ "sus", "no", "nah", "nvm", "false", "rathernot", "nope", "naaah", "naah", "bye", "fart", "sans", "terminal"];
+
+	var alignValues = ["left", "center", "right"];
 
 	var optValues = {
 
@@ -103,13 +111,24 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 	    return optValues[propertyName];
 	};
 
-	var typeVars = ["options", "sizeoptions", "trackingoptions", "opt", "weightoptions", "italicoptions", "size", "font", "tracking", "weight", "italic", "align", "text"];
+	var typeVars = ["sizeoptions", "trackingoptions", "opt", "weightoptions", "italicoptions", "alignoptions", "singlelineoptions", "size", "font", "tracking", "weight", "italic", "align", "singleline", "text"];
 	$.each(typeVars, function(index, value) {
 		if ($(theTester).is("["+value+"]")) {
 		    eval(value + " = '" + $(theTester).attr(value) + "'");
 
 		}
 	});
+
+	if ($(theTester).is("[weightselection]")) {
+	  	weightselection = $(theTester).attr("weightselection").replace(/:/g, ',').split(',');
+	  	weightselection.forEach(function(val, i) {
+		    weightselection[i] = $.trim(val);
+		});
+		weightselection.forEach(function(val, i) {
+		    if (i % 2 === 1) return;
+		    obj_weightselection[val] = weightselection[i + 1];
+		});
+	}
 
 	if ($(theTester).is("[optoptions]")) {
 	  	optoptions = $(theTester).attr("optoptions").replace(/\s+/g, '').split(',');
@@ -138,7 +157,7 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 			return false;
 
 		}else{
-			testerFont = " font-family: " + font + ";";
+			testerFont = " font-family: " + font + ", 'Fakta Grotesk', 'Helvetica Neue', Helvetica, Arial, sans-serif;";
 
 		}
 
@@ -187,7 +206,7 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 			return false;
 
 		}else{
-			testerTracking = " letter-spacing: " + tracking + "px;";
+			testerTracking = " letter-spacing: " + tracking + "em;";
 
 		}
 
@@ -220,11 +239,11 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 			testerWeight = " font-weight: " + weight  + ";";
 
 		}
-
 	}else{
-		testerWeight = " font-weight: normal;";
-		weight = "400";
-
+		if (weightselection == ""){
+			testerWeight = " font-weight: normal;";
+			weight = "400";
+		}
 	}
 
 	if (weightoptions !== ""){
@@ -238,6 +257,37 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 
 		}
 	}
+
+	if (weightselection !== ""){
+		elm += 1;
+		if ($.inArray(weightselection, no) >= 0){
+			weightselection = "";
+			elm -= 1;
+
+		}else{
+			if (weight !== ""){ }else{
+				if ($.inArray(weightselection, no) >= 0){
+					testerWeight = " font-weight: normal;";
+					weight = "400";
+					return false;
+
+				}else{
+					weight = obj_weightselection[Object.keys(obj_weightselection)[0]];
+					testerWeight = " font-weight: " + obj_weightselection[Object.keys(obj_weightselection)[0]] + ";";
+				}
+			}
+
+			weightoptions = "<div class='option slider weightSlider weightSlider" + testerNumber + " weight'><span class='label'>weight</span><span class='amount'></span></div>";
+		}
+	}else{
+		if (weight == ""){
+			testerWeight = " font-weight: normal;";
+			weight = "400";
+		}
+	}
+
+
+
 
 	////////////////////////
 	//ITALICS
@@ -282,14 +332,10 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 			$.each(optoptions, function(index, value) {
 				testerOpt += '<span class="' + value + '">' + optName(value) + '</span>';
 			});
-			optoptions = "<div class='option optButton optButton" + testerNumber + " opt' active='features'><span class='label'>features</span><div class='dropdown'>" +
-						testerOpt +
-					"</div></div>";
+			optoptions = "<div class='option optButton optButton" + testerNumber + " opt' active='features'><span class='label'>features</span><div class='dropdown'>" +testerOpt +"</div></div>";
 			$(theTester).addClass("withOptions");
 
 		}
-
-
 	}
 
 	////////////////////////
@@ -300,12 +346,62 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 		}else{
 			testerAlign = " text-align: left;"
 		}
+
+		if (alignoptions !== ""){
+			elm += 1;
+			if ($.inArray(alignoptions, no) >= 0){
+				alignoptions = "";
+				elm -= 1;
+
+			}else{
+				if (align == ""){
+					align = "center";
+
+				}
+				alignoptions = "<div class='option slider alignSlider alignSlider" + testerNumber + " align'><span class='label'>align</span><span class='amount'></span></div>";
+			}
+		}
+	}
+
+	////////////////////////
+	//Singleline
+	if (size !== ""){
+		if (singleline !== ""){
+			if ($.inArray(singleline, no) >= 0){
+				testerSingleline = " white-space: initial;"
+			}else{
+				testerSingleline = " white-space: nowrap;";
+			}
+		}else{
+			testerSingleline = " white-space: initial;"
+		}
+
+		if (singlelineoptions !== ""){
+			elm += 1;
+			if ($.inArray(singlelineoptions, no) >= 0){
+				singlelineoptions = "";
+				elm -= 1;
+
+			}else{
+				singlelineoptions = "<div class='option button singlelineButton singlelineButton" + testerNumber + " singleline'><span class='label'>singleline</span></div>";
+
+			}
+		}
 	}
 
 
 	////////////////////////
+	//Text Fallback
+	if (font !== "") {
+		if (text == "Try typing here...") {
+			text = "Try " + font;
+		};
+	};
+
+
+	////////////////////////
 	//Put them all together
-	totaloptions = sizeoptions + trackingoptions + weightoptions + italicoptions + optoptions;
+	totaloptions = sizeoptions + trackingoptions + weightoptions + italicoptions + singlelineoptions + alignoptions + optoptions;
 
 	if (options !== "false"){
 		testeroptions = "<div class='options row width" + elm + "'>" +
@@ -313,7 +409,7 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 			"</div>"
 	}
 
-	typetester = "<div class='container " + testerfit + "'><div class='type row' style='" + testerFont + testerSize + testerTracking + testerWeight + testerItalic + testerAlign + "''><span class='enterTXT' spellcheck='false' contenteditable>" + text + "</span></div></div>" + testeroptions;
+	typetester = "<div class='container " + testerfit + "'><div class='type row' style='" + testerFont + testerSize + testerTracking + testerWeight + testerItalic + testerAlign + "''><span class='enterTXT' spellcheck='false' contenteditable style='" + testerSingleline + "'>" + text + "</span></div></div>" + testeroptions;
 	typetesterFont = "<style></style>";
 
 	$(theTester).append(typetester);
@@ -346,41 +442,75 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 		$(".trackingSlider" + testerNumber).slider({
 	      	orientation: "horizontal",
 	      	range: "min",
-	      	min: -10,
-	      	max: 40,
+	      	min: -0.25,
+	      	max: 1,
+	      	step: 0.01,
 	      	value: tracking,
 	      	animate: true,
 		    slide: function(event, ui) {
-		        $(this).find( ".amount" ).text(ui.value + "px");
-		        $(this).parent().parent().find( ".type" ).css("letter-spacing", ui.value + "px");
+		        $(this).find( ".amount" ).text(ui.value + "em");
+		        $(this).parent().parent().find( ".type" ).css("letter-spacing", ui.value + "em");
 		    },
 		   	create: function( event, ui ) {
-		        $(this).find( ".amount" ).text(tracking + "px");
-		        $(this).parent().parent().find( ".type" ).css("letter-spacing", tracking + "px");
+		        $(this).find( ".amount" ).text(tracking + "em");
+		        $(this).parent().parent().find( ".type" ).css("letter-spacing", tracking + "em");
 		    }
 
 	    });
 	};
 
+
 	if (weightoptions !== ""){
+		if (weightselection  == "") {
+			$(".weightSlider" + testerNumber).slider({
+		      	orientation: "horizontal",
+		      	range: "min",
+		      	min: 100,
+		      	max: 900,
+		      	step: 100,
+		      	value: weight,
+		      	animate: true,
+			    slide: function(event, ui) {
+			        $(this).find( ".amount" ).text(ui.value);
+			        $(this).parent().parent().find( ".type" ).css("font-weight", ui.value);
+			    },
+			   	create: function( event, ui ) {
+			        $(this).find( ".amount" ).text(weight);
+			        $(this).parent().parent().find( ".type" ).css("font-weight", weight);
+			    }
+		    });
+	    }
+	};
+
+	var ticker = 0
+	for(i in obj_weightselection) {
+		if (weight == obj_weightselection[i]) {
+			ticker += 1;
+			firstWeightPosition = ticker;
+			firstWeightName = i;
+			firstWeightValue = obj_weightselection[i];
+		}
+	}
+	if (weightselection  !== "") {
 		$(".weightSlider" + testerNumber).slider({
 	      	orientation: "horizontal",
 	      	range: "min",
-	      	min: 100,
-	      	max: 900,
-	      	step: 100,
-	      	value: weight,
+	      	min: 1,
+	      	max: Object.keys(obj_weightselection).length,
+	      	step: 1,
+	      	value: firstWeightPosition,
 	      	animate: true,
 		    slide: function(event, ui) {
-		        $(this).find( ".amount" ).text(ui.value);
-		        $(this).parent().parent().find( ".type" ).css("font-weight", ui.value);
+		        $(this).find( ".amount" ).text(Object.keys(obj_weightselection)[ui.value-1]);
+		        $(this).parent().parent().find( ".type" ).css("font-weight", obj_weightselection[Object.keys(obj_weightselection)[ui.value-1]]);
 		    },
 		   	create: function( event, ui ) {
-		        $(this).find( ".amount" ).text(weight);
-		        $(this).parent().parent().find( ".type" ).css("font-weight", weight);
+		        $(this).find( ".amount" ).text(firstWeightName);
+		        $(this).parent().parent().find( ".type" ).css("font-weight", firstWeightValue);
+
 		    }
 	    });
-	};
+	}
 
 
 	if (italicoptions !== ""){
@@ -399,7 +529,6 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 			};
 	    });
 	};
-
 
 	if (opt !== ""){
 		if ($.inArray(opt, no) >= 0){
@@ -466,6 +595,46 @@ function typetester(e, font, size, tracking, italic,  weight, opt){
 
 	};
 
+
+	if (size !== ""){
+
+		if (singlelineoptions !== ""){
+			if ($(".singlelineButton" + testerNumber).parent().parent().find( ".enterTXT" ).css("white-space") == "nowrap"){
+				$(".singlelineButton" + testerNumber).addClass("active");
+
+			}
+			$(".singlelineButton" + testerNumber).click(function(){
+				$(this).toggleClass("active");
+				if ($(this).hasClass("active")) {
+			    	$(this).parent().parent().find( ".enterTXT" ).css("white-space", "nowrap");
+
+				}else{
+			    	$(this).parent().parent().find( ".enterTXT" ).css("white-space", "initial");
+
+				};
+		    });
+		};
+
+
+		if (alignoptions !== ""){
+			$(".alignSlider" + testerNumber).slider({
+		      	orientation: "horizontal",
+		      	range: "min",
+		      	min: 1,
+		      	max: 3,
+		      	value: alignValues.indexOf(align) + 1,
+		      	animate: true,
+			    slide: function(event, ui) {
+			        $(this).find( ".amount" ).text(alignValues[ui.value - 1]);
+			        $(this).parent().parent().find( ".type" ).css("text-align", alignValues[ui.value - 1]);
+			    },
+			   	create: function( event, ui ) {
+			        $(this).find( ".amount" ).text(align);
+			        $(this).parent().parent().find( ".type" ).css("text-align", align);
+			    }
+			});
+		}
+	}
 }
 
 
